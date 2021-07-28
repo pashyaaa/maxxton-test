fea057b00e638b12b7b4b305b73bcc23ac878c25
Prasad Patil <prasadpatil@janani.com>
2021-07-28 20:57:36 +0530

    Assignment Completed

--- src/app/employee-details/employee-details.component.ts
+++ src/app/employee-details/employee-details.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-details',
   templateUrl: './employee-details.component.html',
   styleUrls: ['./employee-details.component.scss']
 })
-export class EmployeeDetailsComponent implements OnInit {
+export class EmployeeDetailsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  id!: number;
+  employee: any;
+  routeSubscription: Subscription | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    public employeeService: EmployeeService,
+  ) { }
 
   ngOnInit(): void {
+    this.routeSubscription = this.route.paramMap.subscribe(
+      (params) => {
+        this.id = Number(params.get('id'));
+        this.employee = JSON.stringify(this.employeeService.get(this.id));
+      }
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.routeSubscription?.unsubscribe();
   }
 
 }
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import { EmployeeService } from '../employee.service';
 
 @Component({
@@ -9,13 +11,17 @@ import { EmployeeService } from '../employee.service';
 export class EmployeeComponent implements OnInit {
 
   candidate_data = this.employeeService.candidate_data;
-  searchText:string = "";
+  @ViewChild('searchInput')
+  searchInput!: ElementRef<HTMLInputElement>;
+  statisticsString: string = "Not Calculated";
 
   constructor(
-    private employeeService: EmployeeService
+    private employeeService: EmployeeService,
+    private router: Router,
   ) { }
 
   ngOnInit(): void {
+    this.candidate_data.forEach((candidate) => {console.log(new Date(Number(candidate.joining_date.split('/')[2]), Number(candidate.joining_date.split('/')[1]), Number(candidate.joining_date.split('/')[0])).getTime())})
   }
 
   sort(type:string){
@@ -23,21 +29,41 @@ export class EmployeeComponent implements OnInit {
       this.candidate_data.sort((a,b)=>a.name.localeCompare(b.name));
     }
     if(type == 'date'){
-      this.candidate_data.sort((a,b)=> new Date(b.joining_date).getTime() - new Date(a.joining_date).getTime());
+      this.candidate_data.sort((a,b)=> this.processDate(a.joining_date).getTime() - this.processDate(b.joining_date).getTime());
     }
   }
 
-  search(event:any){
-    console.log(event);
-    //this.candidate_data = this.candidate_data.filter((element)=>element.name.indexOf(""));
+  processDate(date:string){
+    return new Date(Number(date.split('/')[2]), Number(date.split('/')[1]), Number(date.split('/')[0]))
   }
 
-  filter(query:string){
+  search(event:any){
+    this.candidate_data = this.employeeService.candidate_data.filter((element)=>element.name.toLowerCase().indexOf(this.searchInput.nativeElement.value.toLowerCase())>-1);
+  }
 
+  filterByYears(){
+    console.log((new Date().getTime() - this.processDate(this.candidate_data[0].joining_date).getTime()) / (1000 * 3600 * 24 * 365));
+    this.candidate_data = this.employeeService.candidate_data.filter((element)=>(new Date().getTime() - this.processDate(element.joining_date).getTime()) / (1000 * 3600 * 24 * 365) < 2);
   }
 
   statistics(){
+    let result:any = {};
+    this.candidate_data.forEach((candidate => {
+      if(result.hasOwnProperty(candidate.department)){
+        result[`${candidate.department}`]++;
+      }else{
+        result[`${candidate.department}`] = 1;
+      }
+    }))
+    this.statisticsString =  JSON.stringify(result);
+  }
 
+  remove(){
+    this.candidate_data = this.employeeService.candidate_data.filter((element)=>element.department.indexOf("Development")==-1);
+  }
+
+  details(id:number){
+    this.router.navigate([`/employee/${id}`])
   }
 
 
